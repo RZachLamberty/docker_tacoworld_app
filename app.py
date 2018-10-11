ab61a26446a2e201d1399c459f0ba96c4d25d525
r.zach.lamberty <r.zach.lamberty@gmail.com>
2018-10-11 18:18:58 -0400

    example docker app initial commit

---
+++ app.py
@@ -0,0 +1,12 @@
+import os
+import socket
+
+# why not?
+import pandas as pd
+
+print('\nHELLO {name}'.format(name=os.getenv("NAME", "world")))
+print('my name is {}'.format(socket.gethostname()))
+print('these are the results of the tacoworld (tm) model')
+print('----------------------\n')
+print("I, Zach Lamberty, want tacos more than *anyone*, especially more than Carlos!")
+print("I, Carlos Blancarte, want tacos, so it's good there are enough to share!")
