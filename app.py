afa4c7b2144127011fc1b114d631aa520a8b88fc
r.zach.lamberty <r.zach.lamberty@gmail.com>
2020-09-27 13:33:19 -0400

    come on it is 2020 already

--- app.py
+++ app.py
@@ -4,9 +4,12 @@ import socket
 # why not?
 import pandas as pd
 
-print('\nHELLO {name}'.format(name=os.getenv("NAME", "world")))
-print('my name is {}'.format(socket.gethostname()))
-print('these are the results of the tacoworld (tm) model')
-print('----------------------\n')
-print("I, Zach Lamberty, want tacos more than *anyone*, especially more than Dan!")
-print("I, Dan Fein, want tacos, so it's good there are enough to share!")
+print(f"""
+HELLO {os.getenv("NAME", "world")}
+my name is {socket.gethostname()}
+these are the results of the tacoworld (tm) model
+----------------------
+
+I, Zach Lamberty, want tacos more than *anyone*, especially more than Eamon!
+I, Eamon Lamberty, want tacos, so it's good there are enough to share!
+""")
